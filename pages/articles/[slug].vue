4ff4a6f10a3ea8c6ccf04faf2f41ee26513d43ee
Thomas Vu <thomasjvu@gmail.com>
2023-01-12 02:33:48 -0500

    Add Page

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -1,30 +1,48 @@
 <template>
-    <NuxtLink
-        class="flex items-center font-fragment font-bold uppercase text-primary-600 hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-200"
-        to="/articles"
-    >
-        <span class="mr-2 text-xl">←</span>
+    <!-- Back to Feed -->
+    <NuxtLink class="flex items-center font-fragment font-bold uppercase mb-5 dark:text-neutral-200" to="/articles">
+        <span class="mr-3 text-xl">←</span>
         Back to Articles
     </NuxtLink>
+    <!-- End of Back to Feed -->
+    <!-- Post Content -->
     <div class="relative pt-96 pb-10 overflow-hidden shadow-xl rounded-2xl">
-        <img class="absolute inset-0 object-cover w-full h-full" :src="fileUrl(article.featured_image)" />
-        <div class="absolute inset-0 bg-primary-500 mix-blend-multiply" />
-        <div class="absolute inset-0 bg-gradient-to-t from-primary-600 via-primary-600 opacity-80" />
+        <img class="absolute inset-0 object-cover w-full h-full" :src="fileUrl(post.featured_image)" />
         <div class="relative px-8">
-            <div class="relative text-lg font-medium ghost font-fugaz md:flex-grow"></div>
+            <div class="relative text-lg font-medium font-fugaz md:flex-grow"></div>
         </div>
     </div>
-    <h1 id="post-title" class="text-5xl sm:text-6xl font-bold drop-shadow-sm">
-        {{ article.title }}
-    </h1>
-    <section id="short-content" class="px-5 py-5 bg-neutral-900 dark:bg-neutral-400 rounded-md darker font-fragment shadow-lg">
-        <p class="text-sm font-extrabold drop-shadow-sm">
-            {{ article.description }}
+    <section id="post-description-container" class="p-5 mt-5 mb-5 bg-neutral-400 rounded-md darker font-fragment shadow-lg text-black">
+        <p class="text-lg">
+            {{ post.description }}
         </p>
     </section>
-    <h4 class="text-sm font-extrabold uppercase drop-shadow-sm">Created By @{{ article.user_created }}</h4>
+    <h1 id="post-title" class="text-5xl sm:text-6xl my-5 font-bold drop-shadow-sm text-center dark:text-neutral-200">
+        {{ post.title }}
+    </h1>
 
-    <section id="author-article" class="font-fragment leading-loose" v-html="article.content" />
+    <section id="" class="font-fragment leading-loose text-xl sm:text-2xl dark:text-neutral-200" v-html="post.content" />
+    <!-- End of Post Content -->
+    <!-- Start of Post Meta -->
+    <section class="post-meta flex flex-col items-center gap-5 mt-20">
+        <h4 class="text-md font-extrabold uppercase drop-shadow-sm dark:text-neutral-200">Created By @{{ post.user_created }}</h4>
+        <!-- Reaction Counts -->
+        <section class="reactions flex">
+            <section class="font-fragment flex flex-col items-center">
+                <Icon name="pixelarticons:mood-happy" size="2rem" />
+                <span>{{ post.positive }}</span>
+            </section>
+            <section class="font-fragment flex flex-col items-center">
+                <Icon name="pixelarticons:mood-neutral" size="2rem" />
+                <span>{{ post.neutral }}</span>
+            </section>
+            <section class="font-fragment flex flex-col items-center">
+                <Icon name="pixelarticons:mood-sad" size="2rem" />
+                <span>{{ post.negative }}</span>
+            </section>
+        </section>
+        <!-- End of Reaction Counts -->
+    </section>
 </template>
 
 <script setup>
@@ -38,7 +56,7 @@ const { params, path } = useRoute()
 // Fetch the page data from the Directus API using the Nuxt useAsyncData composable
 // https://v3.nuxtjs.org/docs/usage/data-fetching#useasyncdata
 const {
-    data: article,
+    data: post,
     pending,
     error,
 } = await useAsyncData(
@@ -48,9 +66,11 @@ const {
     },
     {
         transform: (data) => data.data[0],
-        pick: ['title', 'content', 'description', 'featured_image', 'user_created', 'slug'],
+        pick: ['title', 'content', 'description', 'featured_image', 'user_created', 'slug', 'positive', 'negative', 'neutral'],
     }
 )
 </script>
 
-<style></style>
+<style>
+
+</style>
--- pages/partner.vue
+++ pages/partner.vue
@@ -1,9 +1,17 @@
 <template>
-    <VHeader />
-    <div>
-        <h2 class="text-5xl">Partner Program</h2>
+    <div class="main-container">
+        <Logo />
+        <h2 class="text-5xl font-fragment text-center">Partner Program</h2>
+        <section class="about-container">
+            <h3 class="text-4xl font-fragment">About</h3>
+            <p class="text-xl font-fragment">The OHKO Partner Program is a way for players to earn money from their content.</p>
+        </section>
+        <section class="about-container">
+            <h3 class="text-4xl font-fragment">Eligibility</h3>
+            <p class="text-xl font-fragment">To become eligible for the partner program, you must publish at least 10 articles.</p>
+            <p class="text-xl font-fragment">After you've published at least 10 articles, you can applly for the partner program at this <a href="#">link</a>.</p>
+        </section>
     </div>
-    <VFooter />
 </template>
 
 <script setup>
@@ -15,4 +23,10 @@ useHead({
 </script>
 
 <style>
+.main-container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 25px;
+}
 </style>
