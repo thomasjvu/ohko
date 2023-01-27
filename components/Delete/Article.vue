dadcaf36c2d4bb23c5fde6a942e16b0dd777f220
Thomas Vu <thomasjvu@gmail.com>
2023-01-26 20:56:01 -0500

    Update article delete

---
+++ components/Delete/Article.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script setup>
+import { Directus } from '@directus/sdk'
+
+// const config = useRuntimeConfig()
+const directus = new Directus('https://app.ohko.org')
+const articles = directus.items('articles')
+
+</script>
+
+<style>
+</style>
