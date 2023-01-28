ff9efca9f19fdc4de29a99b9eb280152dc647fdd
Thomas Vu <thomasjvu@gmail.com>
2023-01-27 19:27:05 -0500

    Update dropdown

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -61,7 +61,6 @@
 <script setup>
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/24/solid'
-import VButton from '~~/components/VButton'
 const props = defineProps({
   buttonLabel: {
     type: String,
